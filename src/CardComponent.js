eba0e0942a7f38c4ade0d139c25aa66af119064c
JoshCodedit <joshua.dixoncms@gmail.com>
2024-06-05 12:36:46 +0100

    first wokring game

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -4,6 +4,6 @@ import React from "react";
 export default function CardComponent(props){
     const cardFace = props.isFlipped ? props.image : './images/card-back.png'
     return(
-        <img className="cards" src={cardFace} style={props.show} onClick={props.flipCard}/>
+        <img className="cards" src={cardFace} style={props.show} onClick={props.flipCard} alt="card"/>
     )
 }
